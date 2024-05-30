fdbd248d8fbc38c6d836689d785b735a08bcc71f
unknown <dumbirichuks@gmail.com>
2024-05-30 20:32:08 +0100

    completed the table skeleton, and the bulk delete api action

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -6,15 +6,41 @@ import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import {
     Card,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+import { useGetAccounts } from "@/features/accounts/api/use-get-account";
+import { TableSkeleton } from "@/features/accounts/components/skeleton/table-skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
+
+// const data = [
+//     {
+//         id: "728ed52f",
+//         amount: 100,
+//         status: "pending",
+//         email: "m@example.com",
+//     },
+//     {
+//         id: "728ed52f",
+//         amount: 50,
+//         status: "success",
+//         email: "a@example.com",
+//     },
+// ]
 
 const AccountsPage = () => {
     const { onOpen } = useNewAccount();
+
+    const deleteAccounts = useBulkDeleteAccounts();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    const isDisabled =
+        accountsQuery.isLoading ||
+        deleteAccounts.isPending;
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -31,6 +57,25 @@ const AccountsPage = () => {
                         Add new
                     </Button>
                 </CardHeader>
+                <CardContent>
+                    {accountsQuery.isLoading ?
+                        <>
+                            <TableSkeleton columns={columns} />
+                        </>
+                        : <>
+                            <DataTable
+                                filterKey="email"
+                                columns={columns}
+                                data={accounts}
+                                onDelete={(row) => {
+                                    const ids = row.map((r) => r.original.id);
+                                    deleteAccounts.mutate({ ids })
+                                }}
+                                disabled={isDisabled}
+                            />
+                        </>
+                    }
+                </CardContent>
             </Card>
         </div>
     )
