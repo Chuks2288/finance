f791f828861e9723c6d81fde90190f42a8149fb8
unknown <dumbirichuks@gmail.com>
2024-06-08 02:55:30 +0100

    completed the transactions CSV import, Transactions account select

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Plus } from "lucide-react";
+
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
+import { TableSkeleton } from "@/features/transactions/components/skeleton/table-skeleton";
+import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
 
 import {
     Card,
@@ -9,14 +14,37 @@ import {
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
-import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
-import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
-import { TableSkeleton } from "@/features/transactions/components/skeleton/table-skeleton";
-import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT"
+}
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {},
+}
 
 const TransactionsPage = () => {
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    }
+
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    }
+
     const { onOpen } = useNewTransaction();
 
     const deleteTransactions = useBulkDeleteTransactions();
@@ -27,6 +55,19 @@ const TransactionsPage = () => {
         transactionsQuery.isLoading ||
         deleteTransactions.isPending;
 
+
+    if (variant === VARIANTS.IMPORT) {
+        return (
+            <>
+                <ImportCard
+                    data={importResults.data}
+                    onCancel={onCancelImport}
+                    onSubmit={() => { }}
+                />
+            </>
+        )
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -34,13 +75,18 @@ const TransactionsPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Transactions History
                     </CardTitle>
-                    <Button
-                        size="sm"
-                        onClick={onOpen}
-                    >
-                        <Plus className="size-4 mr-2" />
-                        Add new
-                    </Button>
+                    <div className="flex items-center gap-x-2">
+                        <Button
+                            size="sm"
+                            onClick={onOpen}
+                        >
+                            <Plus className="size-4 mr-2" />
+                            Add new
+                        </Button>
+                        <UploadButton
+                            onUpload={onUpload}
+                        />
+                    </div>
                 </CardHeader>
                 <CardContent>
                     {transactionsQuery.isLoading ?
